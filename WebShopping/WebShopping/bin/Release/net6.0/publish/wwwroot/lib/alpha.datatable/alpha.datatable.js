29d21d92e2a33f53a8c2b332daf9823f30d0b7dd
Mohamed Beshri Amer <mohamed.beshri.amer@gmail.com>
2023-05-26 00:13:46 +0300

    update url with link

---
+++ WebShopping/WebShopping/bin/Release/net6.0/publish/wwwroot/lib/alpha.datatable/alpha.datatable.js
@@ -0,0 +1,35 @@
+﻿function tablePlugin(selector, url, columns) {
+
+    var x = $(selector).DataTable({ //"#citytbl"
+        lengthChange: false,
+        info: true,
+        searching: true,
+        search: true,
+        ordering: false,
+        responsive: true,
+        searchable: true,
+        processing: true,
+        serverSide: true,
+        language: {
+            paginate: {
+                next: "Next",
+                previous: "Pervious"
+            }
+        },
+        ajax: {
+            url: url,
+            async: true,
+            dataSrc: function (json) {
+
+                console.log(json);
+                return json.data;
+            }
+
+        },
+        columns: columns
+
+    });
+    console.log(x);
+    return x;
+
+}
