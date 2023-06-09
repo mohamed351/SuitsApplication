f21a9e6762f0cc89831029e733557dbb3fa28556
Mohamed Beshri Amer <mohamed.beshri.amer@gmail.com>
2023-06-09 19:45:38 +0300

    change url

--- shopping/lib/constaint/constaint.dart
+++ shopping/lib/constaint/constaint.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Constaint {
-  static const String baseURL = "http://192.168.1.6:9092";
+  // static const String baseURL = "http://192.168.1.6:9092";
   // static const String baseURL = "http://192.168.1.2:9092";
-  // static const String baseURL = "http://mohamed85223-001-site1.itempurl.com";
+  static const String baseURL = "http://mohamed85223-001-site1.itempurl.com";
   static const Color primaryColor = Color(0xFF4C53A5);
   static const Color secondaryColor = Color.fromARGB(255, 105, 109, 160);
   static const Color textPrimaryColor = Colors.white;
